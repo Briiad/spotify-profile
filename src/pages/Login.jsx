2c34b706e98fc929531bb87bed1e8b77a1a41065
Brian Aditya <brianadit33@gmail.com>
2022-09-02 08:32:41 +0700

    fix the login issue i hope

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,7 +7,7 @@ import {tapHoverBtn, transition} from '../components/framer'
 const Login = () => {
   return (
     <div className='w-full h-screen flex justify-center items-center shadow-xl'>
-      <motion.a href="https://proftify.vercel.app/login" className='text-white font-bold text-lg 2xl:text-3xl' variants={transition} initial="hidden" animate="visible" exit="exit">
+      <motion.a href="https://damp-fortress-98049.herokuapp.com/login" className='text-white font-bold text-lg 2xl:text-3xl' variants={transition} initial="hidden" animate="visible" exit="exit">
           <motion.button className='bg-primary p-10 2xl:p-16 rounded-full outline-none tracking-widest' variants={tapHoverBtn} whileHover="hover" whileTap="tap">
             LOG IN WITH SPOTIFY
           </motion.button>
